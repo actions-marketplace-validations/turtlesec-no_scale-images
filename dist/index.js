bf4ce955acb5f4d0915033f8c07d9a1ba0199b30
Patricia Aas <psmaas@gmail.com>
2020-09-06 23:07:11 +0200

    Put in the scaling code

--- dist/index.js
+++ dist/index.js
@@ -61,19 +61,100 @@ module.exports = require("tls");
 
 const core = __webpack_require__(310);
 const github = __webpack_require__(462);
+const fs = __webpack_require__(747).promises
+const glob = __webpack_require__(762)
+const path = __webpack_require__(622)
+const gm = __webpack_require__(778)
+const chalk = __webpack_require__(608)
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+  //   `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+  //   Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+// This sets the width of the thumbnails that will be create (if the image is smaller)
+const sizes = [250, 500, 1000]
+
+const im = gm.subClass({ imageMagick: true })
+const images = glob.sync(
+  path.join(process.cwd(), 'images', '**', '*.{jpg,jpeg,png}')
+)
+
+const thumbNameRx = new RegExp(
+  `\\.(${sizes.map(s => `${s}`).join('|')})\\.(jpg|jpeg|png)$`
+)
+console.log(thumbNameRx)
+
+images
+  .filter(name => !thumbNameRx.test(name))
+  .forEach(async imageFile => {
+    const localizedFile = imageFile.replace(process.cwd() + path.sep, '')
+    const image = im(await fs.readFile(imageFile))
+    image.size((err, geometry) => {
+      if (err) {
+        core.setFailed(chalk.red(err))
+        return
+      }
+      const { width } = geometry
+      if (!width) {
+        core.setFailed(chalk.error(`${localizedFile} has no width!`))
+        return
+      }
+      sizes.forEach(async size => {
+        const { dir, name, ext } = path.parse(imageFile)
+        const thumbnailFile = `${dir}${path.sep}${name}.${size}${ext}`
+        if (width <= size) {
+          console.log(
+            chalk.grey(
+              `[${size}] ${localizedFile} size ${width} is smaller or equal`
+            )
+          )
+          fs.copyFile(imageFile, thumbnailFile)
+          return
+        }
+        try {
+          await fs.stat(thumbnailFile)
+          console.log(
+            chalk.cyan(`[${size}] thumbnail for ${localizedFile} exists`)
+          )
+        } catch (_) {
+          image.identify((err, ident) => {
+            if (err) {
+              core.setFailed(chalk.red(err))
+              return
+            }
+            image
+              .resize(size, `${size}>`)
+              .gravity('Center')
+              .noProfile()
+              .strip()
+              .toBuffer(ident.format, async (err, buffer) => {
+                if (err) {
+                  core.setFailed(chalk.red(err))
+                  return
+                }
+                await fs.writeFile(thumbnailFile, buffer)
+                console.log(
+                  chalk.green(
+                    `[${size}] thumbnail for ${localizedFile} created`
+                  )
+                )
+              })
+          })
+        }
+      })
+    })
+  })
+
+console.log(images)
 
 
 /***/ }),
@@ -1988,6 +2069,14 @@ module.exports = isPlainObject;
 /***/ (function(module) {
 
 module.exports = require("http");
+
+/***/ }),
+
+/***/ 608:
+/***/ (function(module) {
+
+module.exports = eval("require")("chalk");
+
 
 /***/ }),
 
@@ -5138,6 +5227,22 @@ exports.RequestError = RequestError;
 /***/ (function(module) {
 
 module.exports = require("zlib");
+
+/***/ }),
+
+/***/ 762:
+/***/ (function(module) {
+
+module.exports = eval("require")("glob");
+
+
+/***/ }),
+
+/***/ 778:
+/***/ (function(module) {
+
+module.exports = eval("require")("gm");
+
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -1,15 +1,96 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs').promises
+const glob = require('glob')
+const path = require('path')
+const gm = require('gm')
+const chalk = require('chalk')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+  //   `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+  //   Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+// This sets the width of the thumbnails that will be create (if the image is smaller)
+const sizes = [250, 500, 1000]
+
+const im = gm.subClass({ imageMagick: true })
+const images = glob.sync(
+  path.join(process.cwd(), 'images', '**', '*.{jpg,jpeg,png}')
+)
+
+const thumbNameRx = new RegExp(
+  `\\.(${sizes.map(s => `${s}`).join('|')})\\.(jpg|jpeg|png)$`
+)
+console.log(thumbNameRx)
+
+images
+  .filter(name => !thumbNameRx.test(name))
+  .forEach(async imageFile => {
+    const localizedFile = imageFile.replace(process.cwd() + path.sep, '')
+    const image = im(await fs.readFile(imageFile))
+    image.size((err, geometry) => {
+      if (err) {
+        core.setFailed(chalk.red(err))
+        return
+      }
+      const { width } = geometry
+      if (!width) {
+        core.setFailed(chalk.error(`${localizedFile} has no width!`))
+        return
+      }
+      sizes.forEach(async size => {
+        const { dir, name, ext } = path.parse(imageFile)
+        const thumbnailFile = `${dir}${path.sep}${name}.${size}${ext}`
+        if (width <= size) {
+          console.log(
+            chalk.grey(
+              `[${size}] ${localizedFile} size ${width} is smaller or equal`
+            )
+          )
+          fs.copyFile(imageFile, thumbnailFile)
+          return
+        }
+        try {
+          await fs.stat(thumbnailFile)
+          console.log(
+            chalk.cyan(`[${size}] thumbnail for ${localizedFile} exists`)
+          )
+        } catch (_) {
+          image.identify((err, ident) => {
+            if (err) {
+              core.setFailed(chalk.red(err))
+              return
+            }
+            image
+              .resize(size, `${size}>`)
+              .gravity('Center')
+              .noProfile()
+              .strip()
+              .toBuffer(ident.format, async (err, buffer) => {
+                if (err) {
+                  core.setFailed(chalk.red(err))
+                  return
+                }
+                await fs.writeFile(thumbnailFile, buffer)
+                console.log(
+                  chalk.green(
+                    `[${size}] thumbnail for ${localizedFile} created`
+                  )
+                )
+              })
+          })
+        }
+      })
+    })
+  })
+
+console.log(images)
